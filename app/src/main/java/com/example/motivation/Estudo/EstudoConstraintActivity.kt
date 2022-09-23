55b2af5c82194aeb2d0acdf647fe1dce8049926a
Rodrigo da Silva <rodrigos262@hotmail.com>
2022-09-23 13:09:37 -0300

    Estudo Constraint Layout

--- app/src/main/java/com/example/motivation/Estudo/EstudoConstraintActivity.kt
+++ app/src/main/java/com/example/motivation/Estudo/EstudoConstraintActivity.kt
@@ -1,7 +1,8 @@
-package com.example.motivation
+package com.example.motivation.Estudo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.motivation.R
 
 class EstudoConstraintActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
